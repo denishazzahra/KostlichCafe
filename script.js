649717d99d1f605be663f28a59c75b62a9fe47cf
Denisha Kyla Azzahra <95396313+denishazzahra@users.noreply.github.com>
2023-08-04 12:07:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//Denisha Kyla Azzahra (123210130)
+var x = window.matchMedia("(max-width: 768px)")
+responsiveness();
+window.onresize=function(event){
+    responsiveness();
+};
+function responsiveness(){
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    var navbar1 = document.getElementById("header");
+    var sticky1 = navbar1.offsetTop;
+    const check=document.getElementById("check");
+    if(!x.matches){
+        navbar1.classList.remove("sticky1");
+        check.innerHTML="";
+        window.onscroll = function() {myFunction()};
+        function myFunction() {
+            if (window.pageYOffset > sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
+    }
+    else{
+        navbar.classList.remove("sticky");
+        check.innerHTML="<br><br><br><br>";
+        window.onscroll = function() {myFunction1()};
+        function myFunction1() {
+            if (window.pageYOffset > sticky1) {
+                navbar1.classList.add("sticky1")
+            } else {
+                navbar1.classList.remove("sticky1");
+            }
+        }
+        const navbarItem = document.querySelector("#navbar-group");
+        navbarItem.addEventListener("click", function () {
+            const navbarGroup = document.querySelector("#navbar-group");
+            navbarGroup.classList.toggle("navbar-group-close");
+        });
+    }
+}
+const navbarIcon = document.querySelector("#navbar-icon");
+navbarIcon.addEventListener("click", function () {
+    const navbarGroup = document.querySelector("#navbar-group");
+    navbarGroup.classList.toggle("navbar-group-close");
+});
